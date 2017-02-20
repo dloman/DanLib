317da95241972e35d3b603b54cd7d7a1023ef8b0
Daniel Loman <dloman@toyon.com>
2017-02-19 16:00:09 -0800

    removed g code stuff from serial test

--- Serial/Test/SerialTest.cpp
+++ Serial/Test/SerialTest.cpp
@@ -8,51 +8,25 @@ std::unique_ptr<dl::serial::Serial> pSerial(nullptr);
 //-----------------------------------------------------------------------------
 int main()
 {
-  std::cout << "Possible Serial Ports:" << std::endl;
   for (const auto& SerialPort : dl::serial::Serial::GetAvailableSerialPorts())
   {
-    std::cout << "- " << SerialPort << std::endl;
-  }
-  std::cout << "\nOpening /dev/ttyACM0" << std::endl;
-
-  try
-  {
-    auto pSerial = std::make_unique<dl::serial::Serial>("/dev/ttyACM0", 115200);
-
-    pSerial->GetOnRxSignal().Connect(
-      [] (const auto& Bytes) {std::cout << Bytes << std::endl;});
-
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    pSerial->Write("G28 Y0\n");
-    std::cout << "going home" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    pSerial->Write("G1 Y500 F15000\n");
-    std::cout << "going right" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    pSerial->Write("G28 Y0\n");
-    std::cout << "going home" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-/*
-    for (auto i = 0; i < 1300; i+=100)
+    try
     {
-      std::string Out = "G1 Y" + std::to_string(i) + " F15000\n";
-      std::cout << "writing " << Out << std::endl;
+      auto pSerial = std::make_unique<dl::serial::Serial>(SerialPort, 115200);
 
-      pSerial->Write(Out);
+      std::cout << "\nconnected to " << SerialPort << std::endl;
+      pSerial->GetOnRxSignal().Connect(
+        [] (const auto& Bytes) {std::cout << "rx = " << Bytes << std::endl;});
 
-      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+      std::string Input;
+      while(std::cin >> Input)
+      {
+        pSerial->Write('!' + Input + '*');
+      }
     }
-
-    pSerial->Write("G28 Y0 F15000\n");*/
-      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-   std::cout << "done" << std::endl;
-  }
-  catch (std::exception& Exception)
-  {
-    std::cerr << "ERROR: Unable to connect to /dev/ttyACM0" << std::endl;
-    return 1;
+    catch (std::exception& Exception)
+    {
+      std::cerr << "ERROR: " << Exception.what() << std::endl;
+    }
   }
 }
