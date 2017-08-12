0493909279dfd01abf4e623b4d83f7be1b18d4d8
Daniel Loman <dloman@toyon.com>
2017-08-12 13:53:19 -0700

    made is continaed recursive

--- TypeTraits/Tests/TypeTraitsTest.cpp
+++ TypeTraits/Tests/TypeTraitsTest.cpp
@@ -6,16 +6,32 @@ int main()
 {
 
   static_assert(
-    dl::ContainsType<int>(std::tuple<double, char, int, unsigned>{}),
+    dl::ContainsType<int, std::tuple<double, char, int, unsigned>>{},
+    "ContainsType didnt work");
+
+  static_assert(
+    !dl::ContainsType<float, std::tuple<double, char, int, unsigned>>{},
+    "ContainsType didnt work");
+
+  static_assert(
+    dl::ContainsType<
+      float,
+      std::tuple<double, char, int, unsigned, std::tuple<std::string, double, float>>>{},
+    "ContainsType didnt work");
+
+  static_assert(
+    !dl::ContainsType<
+      float,
+      std::tuple<double, char, int, unsigned, std::tuple<std::string, double>>>{},
     "ContainsType didnt work");
 
   std::cout <<
     std::boolalpha <<
-    dl::ContainsType<int>(std::tuple<double, char, int, unsigned>{})
+    dl::ContainsType<int, std::tuple<double, char, int, unsigned>> {}
     << std::endl;
 
-  std::cout <<
-    std::boolalpha <<
-    dl::ContainsType<uint8_t>(std::tuple<double, char, int, unsigned>{})
-    << std::endl;
+    std::cout <<
+      std::boolalpha <<
+      dl::ContainsType<uint8_t, std::tuple<double, char, int, unsigned>>{}
+      << std::endl;
 }
--- TypeTraits/TypeTraits/TypeTraits.hpp
+++ TypeTraits/TypeTraits/TypeTraits.hpp
@@ -6,9 +6,44 @@ namespace dl
 {
   //----------------------------------------------------------------------------
   //----------------------------------------------------------------------------
-  template <typename T, typename ... Args>
-  constexpr bool ContainsType(std::tuple<Args...> t)
+  template <typename T, typename Tuple>
+  struct ContainsType;
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  template <typename T>
+  struct ContainsType<T, std::tuple<>>
+  : std::false_type
   {
-    return std::disjunction_v<std::is_same<T, Args>...>;
-  }
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  template <typename T, typename U, typename... Args>
+  struct ContainsType<T, std::tuple<U, Args...>>
+  : ContainsType<T, std::tuple<Args...>>
+  {
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  template <typename T, typename ... Ts, typename... Args>
+  struct ContainsType<T, std::tuple<std::tuple<Ts...>, Args...>>
+  : ContainsType<T, std::tuple<Ts..., Args...>>
+  {
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  template <typename T, typename... Args>
+  struct ContainsType<T, std::tuple<T, Args...>>
+  : std::true_type
+  {
+  };
+
+  template<typename T, typename Tuple>
+  using contains_type_t = typename ContainsType<T, Tuple>::type;
+
+  template<typename T, typename Tuple>
+  using contains_type_v = typename ContainsType<T, Tuple>::value;
 }
